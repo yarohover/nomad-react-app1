9a756a65fd3cb9e4ddf59fdeae93fb1b4b629e69
yaro <skhn00j@gmail.com>
2022-07-13 18:14:11 +0900

    coin tracker 1st

--- src/App.js
+++ src/App.js
@@ -2,38 +2,22 @@ import {useState, useEffect} from "react";
 import "./App.css"
 
 function App() {
-  const [value, setValue] = useState('');
-  const [list, setList] = useState([]);
-
-  const onAdd = event=>{
-    event.preventDefault();
-    if(value === ''){return};
-    const newlist = [value, ...list];
-    setList(newlist);
-    setValue('');
-  };
-
+  const [data, setData] = useState([]);
+  const [load, setLoad] = useState(true);
+  useEffect(()=>{
+    fetch('https://api.coinpaprika.com/v1/tickers')
+    .then(response=>response.json())
+    .then(json=>{setData(json);setLoad(false)});
+  }, [])
   return (
     <div className="App">
-      <h1>to do list</h1>
-      <form onSubmit={onAdd}>
-        <input type="text" value={value} placeholder="to do" onChange={event=>setValue(event.target.value)} />
-        <button type="submit">add</button>
-      </form>
-      <ol>
-        {list.map((elmt,idx)=><li key={idx}>{elmt}<form onSubmit={(event)=>{
-          event.preventDefault();
-          let newlist = [];
-          console.log('elmt',elmt);
-          list.forEach(element => {
-            if(element+'' !== elmt+""){
-              newlist.push(element);
-              console.log(newlist);
-            }
-          });
-          setList(newlist);
-        }}><button type="submit">done</button></form></li>)}
-      </ol>
+      <h1>The Coins</h1>
+      <select>{load?'Loading...':data.map((elmt,idx)=>{
+        return <option key={idx}>
+                {elmt.name}: {elmt.symbol}, ($ {elmt.quotes.USD.price.toFixed(2).toString()
+                .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")})
+               </option>})}
+      </select>
     </div>
   );
 }
